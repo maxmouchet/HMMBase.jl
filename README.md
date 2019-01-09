9c978198887ed48545c367ade2bc34a63c277ef5
Maxime Mouchet <max@maxmouchet.com>
2019-01-09 21:40:12 +0100

    Update README

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
-# HMMBase.jl
+# HMMBase
 
-[![Build Status][travis-img]][travis-url] [![Documentation][docs-latest-img]][docs-latest-url]
+*Hidden Markov Models for Julia.*
+
+| **Documentation**                       | **Build Status**              |
+|:---------------------------------------:|:-----------------------------:|
+| [![][docs-latest-img]][docs-latest-url] | [![][travis-img]][travis-url] |
+
+## Introduction
 
 HMMBase builds upon [Distributions.jl](https://github.com/JuliaStats/Distributions.jl) to provide a lightweight and efficient abstraction for hidden Markov models in Julia.
 It implements the forward and backward recursions, the Viterbi algorithm, and the MLE estimator.
 More advanced models, such as Bayesian HMMs can be built upon HMMBase.
 
-## Usage
+## Installation
+
+The package can be installed with the Julia package manager.
+From the Julia REPL, type `]` to enter the Pkg REPL mode and run:
+
+```
+pkg> add https://github.com/maxmouchet/HMMBase.jl.git
+```
+
+Or, equivalently, via the `Pkg` API:
+
+```julia
+julia> import Pkg; Pkg.add(Pkg.PackageSpec(url="https://github.com/maxmouchet/HMMBase.jl.git"))
+```
+
+## Example
 
 You can run the following example by executing `julia examples/README.jl`.
 
@@ -24,30 +45,13 @@ z, y = sample_hmm(hmm, 2500)
 
 TODO: MLE
 
-## Development
+## Project Status
 
-### Build docs
+The package is tested against Julia 1.0 and the nightly builds of the Julia `master` branch on Linux.
 
-```bash
-julia docs/make.jl
-```
+## Questions and Contributions
 
-```julia
-using Literate
-Literate.markdown("examples/README.jl", "."; documenter=false)
-```
-
-## Maintainers
-
-[@maxmouchet](https://github.com/maxmouchet)
-
-## Contribute
-
-PRs accepted.
-
-## License
-
-MIT © 2018 Maxime Mouchet
+Contributions are very welcome, as are feature requests and suggestions. Please open an [issue][issues-url] if you encounter any problems. In particular additions to documentation are encouraged!
 
 [docs-stable-img]: https://img.shields.io/badge/docs-stable-blue.svg?style=flat
 [docs-stable-url]: https://maxmouchet.github.io/HMMBase.jl/stable
@@ -58,5 +62,6 @@ MIT © 2018 Maxime Mouchet
 [travis-img]: https://travis-ci.org/maxmouchet/HMMBase.jl.svg?branch=master
 [travis-url]: https://travis-ci.org/maxmouchet/HMMBase.jl
 
+[issues-url]: https://github.com/maxmouchet/BaseHMM.jl/issues#-
 *This page was generated using [Literate.jl](https://github.com/fredrikekre/Literate.jl).*
 
--- examples/README.jl
+++ examples/README.jl
@@ -1,12 +1,33 @@
-# # HMMBase.jl
-#
-# [![Build Status][travis-img]][travis-url] [![Documentation][docs-latest-img]][docs-latest-url]
+# # HMMBase
+
+# *Hidden Markov Models for Julia.*
+
+# | **Documentation**                       | **Build Status**              |
+# |:---------------------------------------:|:-----------------------------:|
+# | [![][docs-latest-img]][docs-latest-url] | [![][travis-img]][travis-url] |
+
+# ## Introduction
 
 # HMMBase builds upon [Distributions.jl](https://github.com/JuliaStats/Distributions.jl) to provide a lightweight and efficient abstraction for hidden Markov models in Julia.  
 # It implements the forward and backward recursions, the Viterbi algorithm, and the MLE estimator.  
 # More advanced models, such as Bayesian HMMs can be built upon HMMBase.
 
-# ## Usage
+# ## Installation
+
+# The package can be installed with the Julia package manager.
+# From the Julia REPL, type `]` to enter the Pkg REPL mode and run:
+
+# ```
+# pkg> add https://github.com/maxmouchet/HMMBase.jl.git
+# ```
+
+# Or, equivalently, via the `Pkg` API:
+
+# ```julia
+# julia> import Pkg; Pkg.add(Pkg.PackageSpec(url="https://github.com/maxmouchet/HMMBase.jl.git"))
+# ```
+
+# ## Example
 
 # You can run the following example by executing `julia examples/README.jl`.
 
@@ -22,30 +43,13 @@ z, y = sample_hmm(hmm, 2500)
 
 # TODO: MLE
 
-# ## Development
+# ## Project Status
 
-# ### Build docs
+# The package is tested against Julia 1.0 and the nightly builds of the Julia `master` branch on Linux.
 
-# ```bash
-# julia docs/make.jl
-# ```
+# ## Questions and Contributions
 
-# ```julia
-# using Literate
-# Literate.markdown("examples/README.jl", "."; documenter=false)
-# ```
-
-# ## Maintainers
-
-# [@maxmouchet](https://github.com/maxmouchet)
-
-# ## Contribute
-
-# PRs accepted.
-
-# ## License
-
-# MIT © 2018 Maxime Mouchet
+# Contributions are very welcome, as are feature requests and suggestions. Please open an [issue][issues-url] if you encounter any problems. In particular additions to documentation are encouraged!
 
 # [docs-stable-img]: https://img.shields.io/badge/docs-stable-blue.svg?style=flat
 # [docs-stable-url]: https://maxmouchet.github.io/HMMBase.jl/stable
@@ -55,3 +59,5 @@ z, y = sample_hmm(hmm, 2500)
 #
 # [travis-img]: https://travis-ci.org/maxmouchet/HMMBase.jl.svg?branch=master
 # [travis-url]: https://travis-ci.org/maxmouchet/HMMBase.jl
+
+# [issues-url]: https://github.com/maxmouchet/BaseHMM.jl/issues
