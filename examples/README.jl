56bbe23b58cf278db15582e5f44629c1bbb388bb
Maxime Mouchet <max@maxmouchet.com>
2019-01-10 19:07:10 +0100

    Update README

--- examples/README.jl
+++ examples/README.jl
@@ -4,7 +4,7 @@
 
 # | **Documentation**                       | **Build Status**              |
 # |:---------------------------------------:|:-----------------------------:|
-# | [![][docs-latest-img]][docs-latest-url] | [![][travis-img]][travis-url] |
+# | [![][docs-stable-img]][docs-stable-url] [![][docs-dev-img]][docs-dev-url]| [![][travis-img]][travis-url] |
 
 # ## Introduction
 
@@ -43,6 +43,11 @@ z, y = sample_hmm(hmm, 2500)
 
 # TODO: MLE
 
+# ## Documentation
+
+# - [**STABLE**][docs-stable-url] &mdash; **documentation of the most recently tagged version.**
+# - [**DEVEL**][docs-dev-url] &mdash; *documentation of the in-development version.*
+
 # ## Project Status
 
 # The package is tested against Julia 1.0 and the nightly builds of the Julia `master` branch on Linux.
@@ -54,8 +59,8 @@ z, y = sample_hmm(hmm, 2500)
 # [docs-stable-img]: https://img.shields.io/badge/docs-stable-blue.svg?style=flat
 # [docs-stable-url]: https://maxmouchet.github.io/HMMBase.jl/stable
 # 
-# [docs-latest-img]: https://img.shields.io/badge/docs-latest-blue.svg?style=flat
-# [docs-latest-url]: https://maxmouchet.github.io/HMMBase.jl/latest
+# [docs-dev-img]: https://img.shields.io/badge/docs-dev-blue.svg?style=flat
+# [docs-dev-url]: https://maxmouchet.github.io/HMMBase.jl/dev
 #
 # [travis-img]: https://travis-ci.org/maxmouchet/HMMBase.jl.svg?branch=master
 # [travis-url]: https://travis-ci.org/maxmouchet/HMMBase.jl
