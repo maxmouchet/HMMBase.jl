b4d973866ef8722ef26dcef02a4dc2c189538226
autodocs <autodocs>
2019-01-10 17:52:35 +0000

    build based on 96156d0

---
+++ 0.0.1/search_index.js
@@ -0,0 +1,67 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Home-1",
+    "page": "Home",
+    "title": "Home",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "hmm/#",
+    "page": "HMM",
+    "title": "HMM",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "hmm/#HMMBase.sample_hmm-Tuple{HMM{Univariate},Int64}",
+    "page": "HMM",
+    "title": "HMMBase.sample_hmm",
+    "category": "method",
+    "text": "sample_hmm(hmm::HMM{Univariate}, timesteps::Int)\n\nSample a trajectory... Returns a vector\n\n\n\n\n\n"
+},
+
+{
+    "location": "hmm/#HMMBase.sample_hmm-Tuple{HMM{Multivariate},Int64}",
+    "page": "HMM",
+    "title": "HMMBase.sample_hmm",
+    "category": "method",
+    "text": "sample_hmm(hmm::HMM{Multivariate}, timesteps::Int)\n\nSample a trajectory... Returns a matrix\n\n\n\n\n\n"
+},
+
+{
+    "location": "hmm/#HMM-1",
+    "page": "HMM",
+    "title": "HMM",
+    "category": "section",
+    "text": "sample_hmm(hmm::HMM{Univariate}, timesteps::Int)\nsample_hmm(hmm::HMM{Multivariate}, timesteps::Int)"
+},
+
+{
+    "location": "_index/#",
+    "page": "Index",
+    "title": "Index",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "_index/#Index-1",
+    "page": "Index",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+]}
