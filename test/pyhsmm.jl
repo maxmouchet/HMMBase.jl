b019353e51678a9ec74d8b2e9927fb89a6b4771b
Maxime Mouchet <max@maxmouchet.com>
2019-01-14 17:02:51 +0100

    Update pyhsmm tests

--- test/pyhsmm.jl
+++ test/pyhsmm.jl
@@ -1,8 +1,6 @@
 using Test
 using Random
 using PyCall
-
-push!(LOAD_PATH, "src/")
 using HMMBase
 
 @pyimport pyhsmm
@@ -13,26 +11,29 @@ Random.seed!(42)
 
 trans_matrix = rand(10,10)
 trans_matrix = trans_matrix ./ sum(trans_matrix, dims=2)
-init_dist = ones(10) / 10
+init_distn = ones(10) / 10
 log_likelihoods = rand(2500,10)
 
-ref, _ = pyhsmm.internals[:hmm_states][:HMMStatesPython][:_messages_backwards_normalized](trans_matrix, init_dist, log_likelihoods)
-res1 = messages_backward(trans_matrix, log_likelihoods)
+# Scaled
+ref = pyhsmm.internals[:hmm_states][:HMMStatesPython][:_messages_forwards_normalized](trans_matrix, init_distn, log_likelihoods)
+res = messages_forwards(init_distn, trans_matrix, log_likelihoods)
 
-@test sum(abs.(ref-res1)) < 1e-11
+@test sum(abs.(ref[1]-res[1])) < 1e-11
+@test sum(abs.(ref[2]-res[2])) < 1e-11
+
+ref = pyhsmm.internals[:hmm_states][:HMMStatesPython][:_messages_backwards_normalized](trans_matrix, init_distn, log_likelihoods)
+res = messages_backwards(init_distn, trans_matrix, log_likelihoods)
+
+@test sum(abs.(ref[1]-res[1])) < 1e-11
+@test sum(abs.(ref[2]-res[2])) < 1e-11
+
+# Log
+ref = pyhsmm.internals[:hmm_states][:HMMStatesPython][:_messages_forwards_log](trans_matrix, init_distn, log_likelihoods)
+res = messages_forwards_log(init_distn, trans_matrix, log_likelihoods)
+
+@test sum(abs.(ref-res)) < 1e-8
 
 ref = pyhsmm.internals[:hmm_states][:HMMStatesPython][:_messages_backwards_log](trans_matrix, log_likelihoods)
-res1 = messages_backward_log(trans_matrix, log_likelihoods)
+res = messages_backwards_log(trans_matrix, log_likelihoods)
 
-@test sum(abs.(ref-res1)) < 1e-8
-
-res1a = messages_forward(init_dist, trans_matrix, log_likelihoods)
-res2a, res2b = pyhsmm.internals[:hmm_states][:HMMStatesPython][:_messages_forwards_normalized](trans_matrix, init_dist, log_likelihoods)
-
-@test sum(abs.(res1a-res2a)) < 1e-11
-# @test abs.(res1b-res2b) < 1e-11
-
-res1 = messages_forward_log(init_dist, trans_matrix, log_likelihoods)
-res2 = pyhsmm.internals[:hmm_states][:HMMStatesPython][:_messages_forwards_log](trans_matrix, init_dist, log_likelihoods)
-
-@test sum(abs.(res1-res2)) < 1e-8
+@test sum(abs.(ref-res)) < 1e-8
