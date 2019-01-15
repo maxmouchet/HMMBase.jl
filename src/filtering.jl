c8244a62071e616d6ba32cca2331631d6b6decbe
Maxime Mouchet <max@maxmouchet.com>
2019-01-15 09:32:12 +0100

    Abstract types for messages_*

--- src/filtering.jl
+++ src/filtering.jl
@@ -3,7 +3,7 @@
 
 # Scaled implementations
 
-@views function messages_forwards(init_distn::Vector{Float64}, trans_matrix::Matrix{Float64}, log_likelihoods::Matrix{Float64})
+@views function messages_forwards(init_distn::AbstractVector{Float64}, trans_matrix::AbstractMatrix{Float64}, log_likelihoods::AbstractMatrix{Float64})
     alphas = zeros(size(log_likelihoods))
     logtot = 0.0
 
@@ -30,7 +30,7 @@
     alphas, logtot
 end
 
-@views function messages_backwards(init_distn::Vector{Float64}, trans_matrix::Matrix{Float64}, log_likelihoods::Matrix{Float64})
+@views function messages_backwards(init_distn::AbstractVector{Float64}, trans_matrix::AbstractMatrix{Float64}, log_likelihoods::AbstractMatrix{Float64})
     betas = zeros(size(log_likelihoods))
     betas[end,:] .= 1
     logtot = 0.0
@@ -53,7 +53,7 @@ end
     betas, logtot
 end
 
-function forward_backward(init_distn::Vector{Float64}, trans_matrix::Matrix{Float64}, log_likelihoods::Matrix{Float64})
+function forward_backward(init_distn::AbstractVector{Float64}, trans_matrix::AbstractMatrix{Float64}, log_likelihoods::AbstractMatrix{Float64})
     alphas, _ = messages_forwards(init_distn, trans_matrix, log_likelihoods)
     betas, _ = messages_backwards(init_distn, trans_matrix, log_likelihoods)
     gammas = alphas .* betas
@@ -62,7 +62,7 @@ end
 
 # Log implementations
 
-function messages_forwards_log(init_distn::Vector{Float64}, trans_matrix::Matrix{Float64}, log_likelihoods::Matrix{Float64})
+function messages_forwards_log(init_distn::AbstractVector{Float64}, trans_matrix::AbstractMatrix{Float64}, log_likelihoods::AbstractMatrix{Float64})
     log_alphas = zeros(size(log_likelihoods))
     log_trans_matrix = log.(trans_matrix)
     log_alphas[1,:] = log.(init_distn) .+ log_likelihoods[1,:]
@@ -76,7 +76,7 @@ function messages_forwards_log(init_distn::Vector{Float64}, trans_matrix::Matrix
     log_alphas
 end
 
-function messages_backwards_log(trans_matrix::Matrix{Float64}, log_likelihoods::Matrix{Float64})
+function messages_backwards_log(trans_matrix::AbstractMatrix{Float64}, log_likelihoods::AbstractMatrix{Float64})
     # OPTIMIZE
     log_betas = zeros(size(log_likelihoods))
     log_trans_matrix = log.(trans_matrix)
