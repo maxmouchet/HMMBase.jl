37cef9d4cf28bdca7154d9d2cc01dc2998ffc378
Maxime Mouchet <max@maxmouchet.com>
2019-01-15 10:00:51 +0100

    Temp fix for rand(StaticHMM, ...)

--- src/HMMBase.jl
+++ src/HMMBase.jl
@@ -25,6 +25,17 @@ export
     # utils.jl,
     compute_transition_matrix
 
+# TEMP: https://github.com/JuliaStats/Distributions.jl/issues/812
+function Distributions.allnonneg(x::AbstractArray{T}) where T<:Real
+    for i = 1 : length(x)
+        if !(x[i] >= zero(T))
+            return false
+        end
+    end
+    return true
+end
+Distributions.isprobvec(p::AbstractVector{T}) where {T<:Real} = Distributions.allnonneg(p) && isapprox(sum(p), one(T))
+
 include("hmm.jl")
 include("decoding.jl")
 include("filtering.jl")
