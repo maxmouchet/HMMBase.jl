b8d32c3fa161a5d262dca275d546745d901f597b
Maxime Mouchet <max@maxmouchet.com>
2019-02-03 18:36:13 +0100

    Update discrete obs example

--- examples/discrete_obs.jl
+++ examples/discrete_obs.jl
@@ -4,9 +4,22 @@ using Distributions
 using HMMBase
 using Plots
 
-π = [0.9 0.1; 0.2 0.8]
-D = [Categorical([0.9, 0.1]), Categorical([0.2, 0.8])]
-hmm = HMM(π, D)
+# https://en.wikipedia.org/wiki/Viterbi_algorithm#Example
+
+π0 = [0.6, 0.4]
+π = [0.7 0.3; 0.4 0.6]
+D = [Categorical([0.5, 0.4, 0.1]), Categorical([0.1, 0.3, 0.6])]
+hmm = HMM(π0, π, D)
+#-
 
 z, y = rand(hmm, 250)
-plot(y)
+z_viterbi = viterbi(hmm, y);
+#-
+
+plot(y, linetype=:steppre, label="Observations", size=(600,200))
+#-
+
+plot(z, linetype=:steppre, label="True hidden state", size=(600,200))
+#-
+
+plot(z_viterbi, linetype=:steppre, label="Viterbi decoded hidden state", size=(600,200))
