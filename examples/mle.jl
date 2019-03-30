9053b6c462f0cffd4941f9f1b3d6df9aeb770819
Maxime Mouchet <max@maxmouchet.com>
2019-03-30 12:30:45 +0100

    Documentation: fix MLE example

--- examples/mle.jl
+++ examples/mle.jl
@@ -13,7 +13,7 @@ plot(y, linetype=:steppre, size=(600,200))
 # For now `HMMBase` does not handle the initialization of the parameters.
 # Hence we must instantiate an initial HMM by hand.
 
-hmm = HMM([0.5 0.5; 0.5 0.5], [Normal(0,1), Normal(10,1)])
+hmm = HMM([0.5 0.5; 0.5 0.5], [Normal(-1,1), Normal(15,1)])
 hmm, log_likelihood = fit_mle!(hmm, y, verbose=true)
 #-
 
