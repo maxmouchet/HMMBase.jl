7a0a7727272e7048ad8ed3ce5823443cded9cfdf
Maxime Mouchet <max@maxmouchet.com>
2019-07-17 09:54:15 +0200

    Deps: update to Distributions v0.2.0

--- src/HMMBase.jl
+++ src/HMMBase.jl
@@ -35,18 +35,6 @@ export
     # utils.jl,
     compute_transition_matrix
 
-# TEMP: https://github.com/JuliaStats/Distributions.jl/issues/812
-# TODO: Specify minimum Distributions version in compat
-function Distributions.allnonneg(x::AbstractArray{T}) where T<:Real
-    for i = 1 : length(x)
-        if !(x[i] >= zero(T))
-            return false
-        end
-    end
-    return true
-end
-Distributions.isprobvec(p::AbstractVector{T}) where {T<:Real} = Distributions.allnonneg(p) && isapprox(sum(p), one(T))
-
 include("hmm.jl")
 include("messages.jl")
 include("mle.jl")
