bd13a423a00970d52cf2da0cadea5471a78ded5f
Maxime Mouchet <max@maxmouchet.com>
2019-11-04 14:31:51 +0100

    *_gen.jl -> _api.jl

---
+++ src/mle_api.jl
@@ -0,0 +1,24 @@
+"""
+    fit_mle(hmm, observations; kwargs...) -> AbstractHMM
+
+Estimate the HMM parameters using the EM (Baum-Welch) algorithm.
+
+# Arguments
+- `display::Symbol` (`:none` by default): when to display convergence logs, can be set to `:iter` or `:final`.
+- `init::Symbol` (`:none` by default): if set to `:kmeans` the HMM parameters will be initialized using a K-means clustering.
+- `maxiter::Integer` (100 by default): maximum number of iterations to perform.
+- `tol::Integer` (1e-3 by default): stop the algorithm when the improvement in the log-likelihood is less than `tol`.
+
+# Output
+- `<:AbstractHMM`: a copy of the original HMM with the updated parameters.
+"""
+function fit_mle(hmm::AbstractHMM, observations; init = :none, kwargs...)
+    hmm = copy(hmm)
+
+    if init == :kmeans
+        kmeans_init!(hmm, observations, display = get(kwargs, :display, :none))
+    end
+
+    fit_mle!(hmm, observations; kwargs...)
+    hmm
+end
