3d99841b7db7c57b645ae8b6fab98ce22e649b3b
Maxime Mouchet <max@maxmouchet.com>
2020-06-11 17:12:19 +0200

    Update benchmarks

--- benchmark/README.md
+++ benchmark/README.md
@@ -1,4 +1,6 @@
 ```bash
+pip install future numpy 'scipy==1.1.0'
 pip install hmmlearn pyhsmm
+# Instantiate project, then:
 julia --project=benchmark/ benchmark/make.jl
 ```
---
+++ benchmark/helpers.jl
@@ -0,0 +1,19 @@
+# Recursively create benchmark groups
+function mkgrp!(base, path)
+    path = map(string, path)
+    curr = base
+    for k in path
+        if !(k in keys(curr))
+            curr[k] = BenchmarkGroup()
+        end
+        curr = curr[k]
+    end
+    curr
+end
+
+function mkbench!(f, base, path)
+    grp = mkgrp!(base, path[1:end-1])
+    grp[path[end]] = f()
+end
+
+
